d8025c714af750fded5ccc87583d4af6c2604739
Chandra Wiratama Putera <141868663+Chandrax222@users.noreply.github.com>
2023-11-24 00:24:41 +0700

    Menghitung usia
    Menghitung usia

---
+++ TUGAS 6_1.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tugas 6-1</title>
+</head>
+<body>
+        <?php
+            $aji = 4;
+            $habib = 21;
+            $bintan = 3;
+            echo ("========================================================");
+            echo "<br>";
+            echo ("Usia habib = 21 Tahun ");
+            echo "<br>";
+            echo ("Usia Aji = 4 Tahun ");
+            echo "<br>";
+            $hasil = $aji + $habib;
+            $hasil2 = $hasil + $bintan;
+            echo ("Usia Bintan = $hasil2 "); 
+            echo "<br>";
+            echo ("========================================================");
+        ?>
+</body>
+</html>
